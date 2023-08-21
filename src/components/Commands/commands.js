52c2e9c67518056f1ca2e8456ccd1f8d6115837c
Jocelin Darma <jocelindarma13@gmail.com>
2023-08-21 17:06:25 +0700

    first draft

---
+++ src/components/Commands/commands.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    commands: {
+        about: {
+            description: 'About Me',
+            fn: () => {
+                return "About Me\n---\nMy name is Jocelin Darma. I am a Full Stack Developer and DevOps Enthusiast. I am currently based in Jakarta, Indonesia. My work experience includes building scalable and reliable web applications, slackbots, microservices, as well as automating and supporting to ensure CI/CD in product development. I am a good communicator and self-motivator, able to work as part of a team but also under my own initiative."
+            }
+        },
+        education: {
+            description: 'My Education',
+            fn: () => {
+                return "About Me.\n---\nmale\n18\nstudent\nsolodev\nmelophile\nweeb\ndumb\napathetic\n---\n"
+            }
+        },
+        skills: {
+            description: 'My Tech Skills',
+            fn: () => {
+                return `
+                    these are the skills I have.\n---\n
+                    python           - 90%
+                    java             - 70% 
+                    javascript       - 90%
+                    typescript       - 90%
+                    html5            - 100%
+                    css3             - 100%
+                    go-lang          - 40% [learning]\n---\n
+                `
+            }
+        },
+        projects: {
+            description: 'My Projects',
+            fn: () => {
+                return `
+                    Cool projects I have worked on.\n---\n
+                    'ashterm'                         | 'terminal portfolio'   | 'javascript'
+                    'octocolor'                       | 'github profile color' | 'javascript'
+                    'kanna-chan'                      | 'Discord-Bot'          | 'python'
+                    'wordinal'                        | 'wordle-on-terminal'   | 'javascript'
+                    'weeby.py'                        | 'API-wrapper'          | 'python'
+                    'gitbanner'                       | 'banner-generator'     | 'javascript'\n---\n
+                `
+            }
+        },
+        email: {
+            description: 'Shoot me an email',
+            fn: () => {
+                window.open('https://www.linkedin.com/in/jocelindarma/', '_blank')
+                return "opening mail..."
+            }
+        },
+        linkedin: {
+            description: 'Visit my LinkedIn profile',
+            fn: () => {
+                window.open('https://www.linkedin.com/in/jocelindarma/', '_blank')
+                return "opening linkedin profile..."
+            }
+        },
+        github: {
+            description: 'Visit my GitHub Profile',
+            fn: () => {
+                window.open('https://github.com/jocelindarma', '_blank')
+                return "opening github account..."
+            }
+        },
+        repo: {
+            description: "Visit this website's GitHub repo",
+            fn: () => {
+                window.open("https://github.com/jocelindarma/terminal", '_blank')
+                return "opening repository..."
+            }
+        },
+    },
+    overwrites:{
+        help: {
+            description: 'List all available commands',
+        },
+        clear: {
+            description: 'Clear terminal',
+        },
+    }
+}
---
+++ src/components/Term.js
@@ -0,0 +1,56 @@
+import Terminal from 'react-console-emulator'
+import commands from '../components/Commands/commands.js'
+import React from 'react'
+
+export default function Term() {
+    const cmds = commands.commands
+    const owrs = commands.overwrites
+    const terminal = React.createRef()
+    const [prompt] = React.useState('jocelindarma:~$ ')
+    return (
+        <Terminal
+            ref={terminal}
+            welcomeMessage={[
+                "Welcome!",
+                "---",
+                "Type 'help' to see a list of commands.",
+                "---",
+            ]}
+            commands={{
+                clear: {
+                    description: 'Clears the terminal',
+                    fn: () => {
+                        terminal.current.clearStdout()
+                    }
+                },
+                help: {
+                    description: 'List all available commands',
+                    fn: () => {
+                        return `
+                            ${Object.keys(cmds).map(cmd => `${cmd}${" ".repeat(12-cmd.length)} | ${cmds[cmd].description}${" ".repeat(39-cmds[cmd].description.length)}`).join('\n')}
+                            ${Object.keys(owrs).map(cmd => `${cmd}${" ".repeat(12-cmd.length)} | ${owrs[cmd].description}${" ".repeat(39-owrs[cmd].description.length)}`).join('\n')}
+                        `
+                    }
+                },
+                ...cmds
+            }}
+            promptLabel={prompt} 
+            autoFocus
+            style={{
+                backgroundColor:null,
+                minHeight: null,
+                maxHeight: null,
+                overflow: 'auto',
+                height: '100%',
+                width: '100%',     
+            }}
+            styleEchoBack='fullInherit'
+            contentStyle={{ color: '#ffb86c' , fontWeight: 'normal', paddingLeft: null}} // Text colour
+            promptLabelStyle={{ color: '#ff5555' , fontWeight:'normal'}} // Prompt label colour
+            inputTextStyle={{ color: '#f1fa8c' , fontWeight: 'normal'}}
+            messageStyle={{ color: '#8be9fd' , fontWeight: 'normal', paddingLeft: null}}
+            scrollBehavior='auto'
+            noDefaults
+        />
+    )
+}
